507d414065e972cce3890666e2e7c2cc340c6a40
Peter Darche <pdarche@gmail.com>
2013-01-16 00:31:39 -0500

    add location, food, and physical activity models

--- models.py
+++ models.py
@@ -28,3 +28,101 @@ class AppSettings(Document):
 	khanacademy_consumer_key = StringField()
 	khanacademy_consumer_secret = StringField()
 	cookie_secret = StringField()
+
+
+class Body(Document):
+	date = DateTimeField()
+	user_id = StringField()
+	# sleep
+	# nutrition
+	# physicalActivity
+	# location
+
+
+class PhysicalActivity(Document):
+	date = DateTimeField()
+	user_id = StringField()
+	steps = IntField()
+	distance = FloatField()
+	calories_out = IntField()
+	activity_calories = IntField()
+	floors = IntField()
+	elevation = FloatField()
+	mins_sedentary = IntField()
+	mins_lightly_active = IntField()
+	mins_moderately_active = IntField()
+	mins_highly_active = IntField()
+	active_score = IntField()
+
+
+class Food(Document):
+	createdat = DateTimeField() #needed?
+	user_id = StringField()
+	amount = FloatField()
+	brand = StringField()
+	fitbit_food_id = IntField()
+	fitbit_mealy_type_id = IntField()
+	unit = IntField() #Unit Object?
+	calories = IntField()
+	carbs = FloatField()
+	fat = FloatField()
+	fiber = FloatField()
+	protein = FloatField()
+	sodium = FloatField()
+	sugar = FloatField()
+	water = BooleanField()
+	# image = ImageField()
+	location = GeoPointField()
+	
+
+class Sleep(EmbeddedDocument):
+	date = DateTimeField()
+	user_id = StringField()
+
+
+class VenueContact(EmbeddedDocument):
+	phone = StringField()
+	formattedPhone = StringField()
+	twitter = StringField()
+	facebook = StringField()
+
+
+class VenueLocation(EmbeddedDocument):
+	address = StringField()
+	cross_street = StringField()
+	location = GeoPointField()
+	postal_code = StringField()
+	city = StringField()
+	state = StringField()
+	country = StringField()
+	cc = StringField()
+
+
+class VenueCategory(EmbeddedDocument):
+	name = StringField()
+	plural = StringField()
+	short_name = StringField()
+
+
+class VenueStats(EmbeddedDocument):
+	checkins_count = IntField()
+	users_count = IntField()
+	tip_count = IntField()
+
+
+class Venue(EmbeddedDocument):
+	foursqure_venue_id = StringField()
+	name = StringField()
+	contact = EmbeddedDocumentField(VenueContact)
+	location = EmbeddedDocumentField(VenueLocation)
+	categories = ListField(EmbeddedDocumentField(VenueCategory))
+	stats = EmbeddedDocumentField(VenueStats)
+
+
+class Location(Document):
+	datetime = DateTimeField
+	user_id = StringField()
+	type = StringField() #foursquare or openpaths
+	location = GeoPointField()
+	venue = EmbeddedDocumentField(Venue)
+
